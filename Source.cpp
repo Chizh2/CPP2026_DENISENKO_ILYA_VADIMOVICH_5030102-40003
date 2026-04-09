4e903540d3f73bd625252480a867a7aef818f1d7
Chizh2 <2006ilja@gmail.com>
2026-04-10 00:02:17 +0300

    pravki last

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include "Quadratic.h"
+#include "Queue.h"
+#include "Student.h"
+#include "Teacher.h"
+#include <vector>
+#include <ctime>
+
+int main() {
+	srand(time(nullptr));
+
+	std::vector<Quadratic> tasks = readequations("input.txt");
+
+	std::vector<std::unique_ptr<Student>> students;
+	students.push_back(std::make_unique<GoodStudent>("Inna"));
+	students.push_back(std::make_unique<AverageStudent>("Jack"));
+	students.push_back(std::make_unique<AverageStudent>("Matvey"));
+	students.push_back(std::make_unique<BadStudent>("Max"));
+	students.push_back(std::make_unique<BadStudent>("Anna"));
+	students.push_back(std::make_unique<GoodStudent>("Zakhar"));
+
+	Queue<Letter> mailbox;
+
+	for (const auto& eq : tasks) {
+		for (const auto& student : students) {
+			Letter let(eq, student->solve(eq), student->getname());
+			mailbox.push(let);
+		}
+	}
+
+	Teacher teach;
+	teach.checkall(mailbox);
+	teach.printresults();
+
+	return 0;
+}
+   
